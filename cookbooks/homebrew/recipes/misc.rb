6799859fd812c63c8fdf2d7b0a37de441829f7d1
Gabriel Horner <gabriel.horner@gmail.com>
2012-02-04 14:40:47 -0500

    customize the homebrew packages I want

--- cookbooks/homebrew/recipes/misc.rb
+++ cookbooks/homebrew/recipes/misc.rb
@@ -8,16 +8,6 @@ root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 
-%w(ack xz coreutils imagemagick sqlite wget fortune proctools markdown ctags-exuberant gpgme icu4c wget libyaml).each do |pkg|
+%w(ack tmux readline herrie hunspell sqlite fortune proctools ctags wget libyaml pv tree).each do |pkg|
   homebrew pkg
 end
-
-homebrew "tmux"
-template "#{ENV['HOME']}/.tmux.conf" do
-  mode   0700
-  owner  ENV['USER']
-  group  Etc.getgrgid(Process.gid).name
-  source "dot.tmux.conf.erb"
-  variables({ :home => ENV['HOME'] })
-end
-
