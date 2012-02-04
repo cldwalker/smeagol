23c01416462fc249ead30ea671c439f1eeda0cfe
Gabriel Horner <gabriel.horner@gmail.com>
2012-02-04 17:01:09 -0500

    readline + herrie need to be home homebrewed

--- cookbooks/homebrew/recipes/misc.rb
+++ cookbooks/homebrew/recipes/misc.rb
@@ -8,6 +8,10 @@ root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 
-%w(ack tmux readline herrie hunspell sqlite fortune proctools ctags wget libyaml pv tree).each do |pkg|
+%w(ack tmux hunspell sqlite fortune proctools ctags wget libyaml pv tree).each do |pkg|
   homebrew pkg
 end
+
+# TODO: automate readline + herrie
+# homebrew 'readline' && brew link readline
+# homebrew 'herrie'
